b4b95afb7b98a3f7fad6598518426a36283d4157
sn0w.dll <sn0wfox@national.shitposting.agency>
2024-01-16 11:09:11 -0500

    fix

--- main.asm
+++ main.asm
@@ -2,27 +2,23 @@ section .data
   fname: db "inputfile.txt"   ; file name
 
 section .bss
-  buffer: resb 500          ; reserve 500 byte buffer
   stat:   resb 144          ; reserve 144 bytes for struct stat
 
 section .text
   global _start
 
 _start:
-  mov rax, 2                ; syscall open()
-  mov rdi, fname            ; file name
-  mov rsi, 0                ; mode (read)
+  mov rax, 2                ; open
+  mov rdi, [rsp+8]          ; filename in argv[1]
+  mov rsi, 0                ; mode
   mov rdx, 0666             ; permissions
-  syscall                   ; returns file descriptor in rax
+  syscall
 
-  mov rdi, rax              ; move fd into rdi
-  mov rax, 5                ; syscall fstat()
+  mov rdi, rax
+  mov rax, 5                ; fstat
   lea rsi, stat
   syscall
 
-  ; set gdb breakpoint here
-  mov rax, [stat+48]         ; move st_size offset into rax
-
-  mov rax, 60               ; syscall exit()
-  mov rdi, 0                ; exit code 0
+  mov rax, 60               ; exit
+  mov rdi, 0
   syscall
